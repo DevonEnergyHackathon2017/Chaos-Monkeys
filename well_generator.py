5e3a6c9627d69a7533635ad1895ab8ea34b16fff
Paul Poputa-Clean <Paul.Poputa-Clean@dvn.com>
2017-11-30 13:31:34 -0600

    spreading the data

--- well_generator.py
+++ well_generator.py
@@ -6,6 +6,7 @@ import random
 import json
 import sys
 import os
+import time
 
 from azure.servicebus import ServiceBusService
 
@@ -16,6 +17,7 @@ sbs = ServiceBusService(service_namespace='chaosMonkeys', shared_access_key_name
 files = ['calculated-bh-sand-conc.json', 'fr-breaker.json', 'slurryrate.json', 'crosslinker.json', 'gelling-agent.json', 'pressure.json', 'surface-sand-conc.json']
 
 pi_data = {}
+max=0
 for datafile in files:
     fn = os.path.join(os.path.dirname(__file__), 'well-values/' + datafile)
     items = json.load(open(fn))
@@ -30,6 +32,7 @@ for i in range(1000):
     for k in pi_data.keys():
         value = pi_data[k][i]['Value']
         row[k] = value
+    time.sleep(0.5)
     sbs.send_event('fracjob', json.dumps(row))
     #print(row)
     #input()
